7a2b5a2ffcc43d16ff221b84c2d21c4b71c9d227
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2025-01-07 08:08:13 +0200

    Extracted Questions class

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -1,55 +1,24 @@
 package trivia;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 import static java.lang.System.out;
 
 // REFACTOR ME
 public class Game implements IGame {
-   public static final int QUESTION_COUNT = 50;
+
    public static final int BOARD_SIZE = 12;
    public static final int MAX_PLAYERS = 6;
    public static final int WINNING_COINS = 6;
-
-   public enum Category {
-      POP, SCIENCE, SPORTS, ROCK;
-
-      @Override
-        public String toString() {
-             return name().charAt(0) + name().substring(1).toLowerCase();
-        }
-   }
+   final Questions questions = new Questions();
 
    ArrayList<String> players = new ArrayList<>();
    int[] places = new int[MAX_PLAYERS];
    int[] coins = new int[MAX_PLAYERS];
    boolean[] inPenaltyBox = new boolean[MAX_PLAYERS];
 
-   LinkedList<String> popQuestions = new LinkedList<>();
-   LinkedList<String> scienceQuestions = new LinkedList<>();
-   LinkedList<String> sportsQuestions = new LinkedList<>();
-   LinkedList<String> rockQuestions = new LinkedList<>();
-
    int currentPlayer = 0;
    boolean isGettingOutOfPenaltyBox;
-
-   public Game() {
-      populateQuestions();
-   }
-
-   private void populateQuestions() {
-      for (int i = 0; i < QUESTION_COUNT; i++) {
-         popQuestions.addLast(createQuestion(i, Category.POP));
-         scienceQuestions.addLast(createQuestion(i, Category.SCIENCE));
-         sportsQuestions.addLast(createQuestion(i, Category.SPORTS));
-         rockQuestions.addLast(createQuestion(i, Category.ROCK));
-      }
-   }
-
-   public String createQuestion(int index, Category category) {
-      return category + " Question " + index;
-   }
 
    public boolean add(String playerName) {
       players.add(playerName);
@@ -81,8 +50,8 @@ public class Game implements IGame {
             out.println(players.get(currentPlayer)
                                + "'s new location is "
                                + places[currentPlayer]);
-            out.println("The category is " + currentCategory(places[currentPlayer]));
-            askQuestion(places[currentPlayer]);
+            out.println("The category is " + questions.currentCategory(places[currentPlayer]));
+            questions.askQuestion(places[currentPlayer]);
          } else {
             out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
             isGettingOutOfPenaltyBox = false;
@@ -96,30 +65,10 @@ public class Game implements IGame {
          out.println(players.get(currentPlayer)
                             + "'s new location is "
                             + places[currentPlayer]);
-         out.println("The category is " + currentCategory(places[currentPlayer]));
-         askQuestion(places[currentPlayer]);
+         out.println("The category is " + questions.currentCategory(places[currentPlayer]));
+         questions.askQuestion(places[currentPlayer]);
       }
 
-   }
-
-   private void askQuestion(int place) {
-      switch (currentCategory(place)) {
-         case POP -> out.println(popQuestions.removeFirst());
-         case SCIENCE -> out.println(scienceQuestions.removeFirst());
-         case SPORTS -> out.println(sportsQuestions.removeFirst());
-         case ROCK -> out.println(rockQuestions.removeFirst());
-          default -> throw new IllegalStateException("Unexpected value: " + currentCategory(place));
-      }
-   }
-
-
-   private Category currentCategory(int place) {
-      return switch (place % 4) {
-         case 0 -> Category.POP;
-         case 1 -> Category.SCIENCE;
-         case 2 -> Category.SPORTS;
-         default -> Category.ROCK;
-      };
    }
 
    public boolean wasCorrectlyAnswered() {
@@ -170,7 +119,6 @@ public class Game implements IGame {
       if (currentPlayer == players.size()) currentPlayer = 0;
       return true;
    }
-
 
    private boolean didPlayerWin() {
       return coins[currentPlayer] != WINNING_COINS;
---
+++ src/main/java/trivia/Questions.java
@@ -0,0 +1,66 @@
+package trivia;
+
+import java.util.LinkedList;
+
+import static java.lang.System.out;
+
+import static trivia.Questions.Category.POP;
+import static trivia.Questions.Category.SCIENCE;
+import static trivia.Questions.Category.SPORTS;
+import static trivia.Questions.Category.ROCK;
+
+public class Questions {
+    public static final int QUESTION_COUNT = 50;
+    
+    LinkedList<String> popQuestions = new LinkedList<>();
+    LinkedList<String> scienceQuestions = new LinkedList<>();
+    LinkedList<String> sportsQuestions = new LinkedList<>();
+    LinkedList<String> rockQuestions = new LinkedList<>();
+
+    public enum Category {
+        POP, SCIENCE, SPORTS, ROCK;
+
+        @Override
+        public String toString() {
+            return name().charAt(0) + name().substring(1).toLowerCase();
+        }
+    }
+
+    public Questions() {
+        populateQuestions();
+    }
+
+    void populateQuestions() {
+        for (int i = 0; i < QUESTION_COUNT; i++) {
+            popQuestions.addLast(createQuestion(i, POP));
+            scienceQuestions.addLast(createQuestion(i, SCIENCE));
+            sportsQuestions.addLast(createQuestion(i, SPORTS));
+            rockQuestions.addLast(createQuestion(i, ROCK));
+        }
+    }
+
+    public String createQuestion(int index, Category category) {
+        return category + " Question " + index;
+    }
+
+    void askQuestion(int place) {
+        switch (currentCategory(place)) {
+            case POP -> out.println(popQuestions.removeFirst());
+            case SCIENCE -> out.println(scienceQuestions.removeFirst());
+            case SPORTS -> out.println(sportsQuestions.removeFirst());
+            case ROCK -> out.println(rockQuestions.removeFirst());
+            default -> throw new IllegalStateException("Unexpected value: " + currentCategory(place));
+        }
+    }
+
+    Category currentCategory(int place) {
+        return switch (place % 4) {
+            case 0 -> POP;
+            case 1 -> SCIENCE;
+            case 2 -> SPORTS;
+            default -> ROCK;
+        };
+    }
+
+
+}
